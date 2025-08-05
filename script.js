3e78285ae5a78733f7f8412762cf0ecb250ae5f0
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-08-05 10:38:07 +0530

    script.css uploaded

---
+++ script.js
@@ -0,0 +1,33 @@
+const allCheckboxList = document.querySelectorAll('.custom-checkbox')
+const inputFields = document.querySelectorAll('.goal-input')
+const errorLabel = document.querySelector('.error-label');
+const progressBar = document.querySelector('.progress-bar');
+const progressValue = document.querySelector('.progress-value');
+
+
+
+
+allCheckboxList.forEach((checkbox) => {
+    checkbox.addEventListener('click' , (e) => {
+        const allGoalAdded = [...inputFields].every(function(input) {
+            return input.value;
+        });
+
+
+        if(allGoalAdded) {
+            checkbox.parentElement.classList.toggle('completed');
+            progressValue.style.Nwidth = '33.3%'
+        }
+        else{
+            progressBar.classList.add('show-error')
+        }
+    })
+})
+
+
+
+inputFields.forEach((input) => {
+    input.addEventListener(('focus') , () => {
+        progressBar.classList.remove('show-error');
+    })
+})
